e0c0d8a394a5282d5b69a8ac77ab66e8aabb74fd
neznaika94 <andreyspbsu94@gmail.com>
2015-03-13 00:12:51 +0300

    can choose text size in second activity

--- gen/ru/android/bashviewer/R.java
+++ gen/ru/android/bashviewer/R.java
@@ -32,9 +32,9 @@ public final class R {
         public static final int action_settings=0x7f090005;
         public static final int btn=0x7f090002;
         public static final int btn2=0x7f090001;
-        public static final int button1=0x7f090003;
-        public static final int scrollView1=0x7f090004;
-        public static final int textView1=0x7f090000;
+        public static final int btn3=0x7f090003;
+        public static final int scrollView=0x7f090004;
+        public static final int txtView=0x7f090000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -48,6 +48,7 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int btn2_text=0x7f050005;
+        public static final int btn3_text=0x7f050007;
         public static final int btn_text=0x7f050004;
         public static final int hello_world=0x7f050001;
         public static final int status_text=0x7f050003;
--- src/ru/android/bashviewer/ActivityWithText.java
+++ src/ru/android/bashviewer/ActivityWithText.java
@@ -1,15 +1,32 @@
 package ru.android.bashviewer;
 
 import android.app.Activity;
+import android.hardware.Camera.Size;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ActivityWithText extends Activity {
+	final int SIZE_10 = 0;
+	final int SIZE_12 = 1;
+	final int SIZE_14 = 2;
+	final int SIZE_16 = 3;	
+	final int SIZE_18 = 4;	
+	
+	private TextView txtView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.withtext);
+		
+		txtView = (TextView)findViewById(R.id.txtView);
+		registerForContextMenu(txtView);
 	}	
 	@Override
 	protected void onStart() {
@@ -22,4 +39,42 @@ public class ActivityWithText extends Activity {
 		super.onStop();
 		//saved current state
 	}
+	
+	@Override 
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		if (v.getId() == R.id.txtView) {
+			menu.add(0, SIZE_10, 0, "10");
+			menu.add(0, SIZE_12, 0, "12");
+			menu.add(0, SIZE_14, 0, "14");
+			menu.add(0, SIZE_16, 0, "16");
+			menu.add(0, SIZE_18, 0, "18");
+		}
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		int newSize = 20;
+		switch(item.getItemId()) {
+		case SIZE_10:
+			newSize = 10;
+			break;
+		case SIZE_12:
+			newSize = 12;
+			break;
+		case SIZE_14:
+			newSize = 14;
+			break;
+		case SIZE_16:
+			newSize = 16;
+			break;	
+		case SIZE_18:
+			newSize = 18;
+			break;	
+		}
+		
+		txtView.setTextSize(newSize);
+
+		return super.onMenuItemSelected(featureId, item);
+	}
+
 }
